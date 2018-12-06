5f48ff07789818769f4fb6a5f3225d3346a36602
Bobby Becker <bobby@68-168-167-10.fttp.usinternet.com>
2018-12-05 21:24:05 -0600

    adds initial markup and stylesheet

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
+app.get("/style.css", function (req, res) {
+  res.sendFile(path.join(__dirname + "/style.css"));
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
